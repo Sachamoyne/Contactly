21e28a75b6029a81b99af7fe9ec5e11e008ab9a1
Sacha Moyne <sach.moyne@gmail.com>
2026-02-12 10:06:33 +0100

    Initial Commit

---
+++ Contactly/ContactlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactlyApp.swift
+//  Contactly
+//
+//  Created by Sacha Moyne  on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ContactlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactlyTests/ContactlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactlyTests.swift
+//  ContactlyTests
+//
+//  Created by Sacha Moyne  on 12/02/2026.
+//
+
+import Testing
+@testable import Contactly
+
+struct ContactlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
